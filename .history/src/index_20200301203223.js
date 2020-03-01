92ecc203b89d357f3a803cc5dc2abf007c4d771b
ArtemisWang <xiao2sheng@qq.com>
2020-03-01 20:50:44 +0800

    art

---
+++ .history/src/index_20200301203223.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import {} from 'react-redux'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
