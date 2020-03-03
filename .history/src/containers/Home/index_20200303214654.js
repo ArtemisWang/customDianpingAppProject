ed6e98cba70ee5dde4f9c64083e9eabb99eabb44
ArtemisWang <xiao2sheng@qq.com>
2020-03-03 23:00:52 +0800

    art

--- .history/src/containers/Home/index_20200303214654.js
+++ .history/src/containers/Home/index_20200303214654.js
@@ -4,22 +4,15 @@ import Headline from './components/Headline'
 import Discount from './components/Discount'
 import LikeList from './components/LikeList'
 import HomeHeader from './components/HomeHeader/'
-import Banner from './components/Banner/'
-import Activity from './components/Activity/'
-import Footer from '../../components/Footer/'
 
 class Home extends Component {
     render() { 
         return ( 
             <div>
-                <HomeHeader/>
-                <Banner/>
                 <Category/>
                 <Headline/>
-                <Activity/>
                 <Discount/>
                 <LikeList/>
-                <Footer/>
             </div>
          );
     }
