e92f94c4adbebd88af2000f43bb01895c19b31ca
ArtemisWang <xiao2sheng@qq.com>
2020-03-03 21:16:13 +0800

    art

---
+++ .history/src/containers/Home/components/Hseadline/index_20200303161153.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick'
+
+class Headline extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+
+        </div> );
+    }
+}
+ 
+export default Headline;
