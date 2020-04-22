4915f2c7d5427d2711004efebe0470d6bf474e4e
ArtemisWang <xiao2sheng@qq.com>
2020-04-22 21:51:07 +0800

    art

--- src/utils/AsyncComponent.js
+++ src/utils/AsyncComponent.js
@@ -10,16 +10,18 @@ export default function asyncComponent(importComponent) {
     }
 
     componentDidMount() {
-      importComponent().then((mod) => {
+      importComponent() // 我们传进来的函数返回我们想要的组件B
+      .then((mod) => {
         this.setState({
-          component: mod.default
+          component: mod.default  // 把组件B存起来
         })
+        // console.log(mod)
       })
     }
 
     render() {
-      const C = this.state.component;
-      return C ? <C {...this.props} /> : null
+      const C = this.state.component; // 渲染的时候把组件B拿出来
+      return C ? <C {...this.props} /> : null  // 返回的其实就是组件B，并且把传给A的属性也转移到B上
     }
   }
 
